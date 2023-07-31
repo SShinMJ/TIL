6286448259e0da5ff5e392fb786a66c04858d4c3
SShinMJ <82142527+SShinMJ@users.noreply.github.com>
2023-07-31 14:05:57 +0900

    Add: SDL2

---
+++ Unity/Study Project/Unity 엔진 기본 구조 구현/SDL 구현/Program.cs
@@ -0,0 +1,92 @@
+using SDL2;
+using System;
+using static SDL2.SDL;
+
+namespace L20230725
+{
+    class Program
+    {
+        static int[,] map = {
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 1, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }
+            };
+        static void Main(string[] args)
+        {
+            Engine myEngine = Engine.GetInstance();
+
+            for (int y = 0; y < 10; ++y)
+            {
+                for (int x = 0; x < 10; ++x)
+                {
+                    if (map[y, x] == 1)
+                    {
+                        GameObject floor = new GameObject();
+                        floor.name = "floor";
+                        floor.transform.x = x;
+                        floor.transform.y = y;
+                        floor.AddComponent(new MeshFilter(' '));
+                        floor.AddComponent(new MeshRenderer(255, 255, 255, 0, "floor.bmp"));
+                        myEngine.Instanciate(floor);
+
+                        GameObject wall = new GameObject();
+                        wall.name = "wall";
+                        wall.transform.x = x;
+                        wall.transform.y = y;
+                        wall.AddComponent(new MeshFilter('*'));
+                        wall.AddComponent(new MeshRenderer(0, 0, 255, 0, "wall.bmp"));
+                        wall.AddComponent(new Collider());
+                        myEngine.Instanciate(wall);
+                    }
+                    else if (map[y, x] == 0)
+                    {
+                        GameObject floor = new GameObject();
+                        floor.name = "floor";
+                        floor.transform.x = x;
+                        floor.transform.y = y;
+                        floor.AddComponent(new MeshFilter(' '));
+                        floor.AddComponent(new MeshRenderer(255, 255, 255, 0, "floor.bmp"));
+                        myEngine.Instanciate(floor);
+                    }
+                }
+
+            }
+
+            GameObject goal = new GameObject();
+            goal.name = "goal";
+            goal.transform.x = 8;
+            goal.transform.y = 8;
+            goal.AddComponent(new MeshFilter('G'));
+            goal.AddComponent(new MeshRenderer(0, 255, 255, 0, "coin.bmp"));
+            goal.AddComponent(new GoalIn());
+            myEngine.Instanciate(goal);
+
+            GameObject monster = new GameObject();
+            monster.name = "monster";
+            monster.transform.x = 7;
+            monster.transform.y = 7;
+            monster.AddComponent(new MeshFilter('M'));
+            monster.AddComponent(new MeshRenderer(0, 255, 0, 255, "slime.bmp"));
+            monster.AddComponent(new AIController());
+            myEngine.Instanciate(monster);
+
+            GameObject player = new GameObject();
+            player.name = "player";
+            player.transform.x = 1;
+            player.transform.y = 1;
+            player.AddComponent(new MeshFilter('P'));
+            player.AddComponent(new MeshRenderer(255, 0, 0, 0, "test.bmp"));
+            player.AddComponent(new PlayerController());
+            myEngine.Instanciate(player);
+
+            myEngine.Run();
+        }
+    }
+}
